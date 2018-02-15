b13a7185cbf249b6a661be54fcd3ffb52dda7262
varady <varady@iit.uni-miskolc.hu>
2018-02-15 12:43:21 +0100

    hello word

---
+++ src/my/first/HelloWord.java
@@ -0,0 +1,12 @@
+package my.first;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message = "Hello word";
+		System.out.println(message);
+
+	}
+
+}
