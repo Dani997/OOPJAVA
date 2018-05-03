b597ebb594078d9133cd527bdc08f2de3cc7eac7
Dani997 <36503822+Dani997@users.noreply.github.com>
2018-05-03 14:07:49 +0200

    Add files via upload

---
+++ oopjava/HelloWorld/src/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args argumentum
+	 */
+	public static void main(String[] args) {
+		//Komment
+		//Komment3
+		/*
+		 * Kommentek
+		 * XYZ
+		 */
+		
+		System.out.println("Hello Anita!");
+
+	}
+
+}
